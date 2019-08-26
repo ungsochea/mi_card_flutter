321a41e7b722d6aa5b5198cc97990031c7969466
ungsochea <sochea.ung@gmail.com>
2019-08-26 11:48:45 +0700

    Card use with ListTile and Completed App

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
         backgroundColor:Colors.teal,
         body:SafeArea(
           child: Column(
-            // crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
@@ -24,30 +24,23 @@ class MyApp extends StatelessWidget {
                 fontWeight: FontWeight.bold,
                 ),),
               Text('FLUTTER DEVELOPER',style: TextStyle(fontFamily: 'Source Sans Pro',color: Colors.teal.shade100,fontSize: 20.0,letterSpacing: 2.5,fontWeight: FontWeight.bold),),
-              Container(
-                padding: EdgeInsets.all(10.0),
+              SizedBox(width: 150.0,height: 20.0,child: Divider(color: Colors.teal.shade100,),),
+              Card(
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(Icons.phone,color: Colors.teal,),
-                    SizedBox(width: 10.0,),
-                    Text('+855 99 666 432',style: TextStyle(color: Colors.teal.shade900,fontFamily: 'Source Sans Pro',fontSize: 20.0),),
-                  ],
+                child: ListTile(
+                  leading: Icon(Icons.phone,color:Colors.teal),
+                  title: Text('+855 99 666 432',style: TextStyle(color: Colors.teal.shade900,fontFamily: 'Source Sans Pro',fontSize: 20.0),),
                 ),
               ),
-              Container(
+              Card(
                 color: Colors.white,
-                padding: EdgeInsets.all(10.0),
                 margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(Icons.email,color: Colors.teal,),
-                    SizedBox(width: 10.0,),
-                    Text('sochea.ung@gmail.com',style: TextStyle(fontSize: 20.0,color:Colors.teal.shade900,fontFamily: 'Source Sans Pro',),)
-                  ],
-                ),
-              )
+                child: ListTile(
+                  leading: Icon(Icons.email,color: Colors.teal,),
+                  title: Text('sochea.ung@gmail.com',style: TextStyle(fontSize: 20.0,color:Colors.teal.shade900,fontFamily: 'Source Sans Pro',),),
+                )
+              ),
               ],
             )
           )
